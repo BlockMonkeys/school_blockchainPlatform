db6afafbd75b2c8ccfd5cf136f0aa238747053ba
blockmonkey <blockmonkey@naver.com>
2021-08-07 11:55:16 +0900

    block

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	//bc := NewBlockchain()
+	//
+	//bc.AddBlock("Send 1 Token to NAMEONE")
+	//bc.AddBlock("Send 2 Token to NAMETWO")
+	//
+	//for _, block := range bc.blocks {
+	//	fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
+	//	fmt.Printf("Data: %s\n", block.Data)
+	//	fmt.Printf("Hash: %x\n", block.Hash)
+	//	fmt.Println()
+	//}
+
+	cli := CLI{}
+	cli.Run()
+}
